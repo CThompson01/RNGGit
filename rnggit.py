9b829c2716f35036e55cfecfaccd572e904d7004
Chase Thompson <nuclearpheasant@hotmail.com>
2017-09-06 22:32:02 -0500

    Render Image
    Program renders 5x5 images depicting a randomized image.

--- rnggit.py
+++ rnggit.py
@@ -1,19 +1,26 @@
 import random
+from PIL import Image
 
-int = [[False, False, False, False, False],[False, False, False, False, False]]
-middle = [False,False,False,False,False]
+image_rgb_data = [[False, False, False, False, False],[False, False, False, False, False],[False,False,False,False,False],[False, False, False, False, False],[False,False,False,False,False]]
 def generateImage():
-    for i in range(0, len(int)):
-        for x in range(0, len(int[i])):
+    for i in range(0, 2):
+        for x in range(0, len(image_rgb_data[i])):
             if random.randint(0,1) == 1:
-                int[i][x] = True
-        print int[i]
-    for i in range(0, len(middle)):
+                image_rgb_data[i][x] = True
+    for i in range(0, len(image_rgb_data[2])):
         if random.randint(0,1) == 1:
-            middle[i] = True
-    print middle
-    print int[1]
-    print int[0]
-            
-            
-        
+            image_rgb_data[2][i] = True
+    image_rgb_data[3] = image_rgb_data[1]
+    image_rgb_data[4] = image_rgb_data[0]
+    
+    showImage()
+    
+
+def showImage():
+    img = Image.new( 'RGB', (5,5), "white")
+    pixels = img.load()
+    for i in range(img.size[0]):
+        for x in range(img.size[1]):
+            if (image_rgb_data[i][x]):
+                pixels[i,x] = (175, 175, 100)
+    img.show()
