499b1b9beed6129bc2b69be3b6f68f38a028cc44
Chase Thompson <Nuclearpheasant@hotmail.com>
2017-09-08 08:51:42 -0500

    Enlarge Image Size

--- rnggit.py
+++ rnggit.py
@@ -5,11 +5,9 @@ image_rgb_data = [[False, False, False, False, False],[False, False, False, Fals
 def generateImage():
     for i in range(0, 2):
         for x in range(0, len(image_rgb_data[i])):
-            if random.randint(0,1) == 1:
-                image_rgb_data[i][x] = True
+            image_rgb_data[i][x] = random.randint(0,50) >= 30
     for i in range(0, len(image_rgb_data[2])):
-        if random.randint(0,1) == 1:
-            image_rgb_data[2][i] = True
+        image_rgb_data[2][i] = random.randint(0,50) >= 33
     image_rgb_data[3] = image_rgb_data[1]
     image_rgb_data[4] = image_rgb_data[0]
     
@@ -17,11 +15,11 @@ def generateImage():
     
 
 def showImage():
-    img = Image.new( 'RGB', (5,5), "white")
+    img = Image.new( 'RGB', (500,500), "white")
     pixels = img.load()
     for i in range(img.size[0]):
         for x in range(img.size[1]):
-            if (image_rgb_data[i][x]):
+            if (image_rgb_data[int(i/100)][int(x/100)]):
                 pixels[i,x] = (175, 175, 100)
     img.show()
-    img.save('C:/Users/ct137324/Desktop/RNGGit/' + str(random.randint(0, 999999999)) + '.png')
+    img.save('images/' + str(random.randint(0, 999999999)) + '.png')
